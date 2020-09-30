5812b6da70da5c9278f1f24bf4e8310324b478e4
vviittaall123 <71178421+vviittaall123@users.noreply.github.com>
2020-10-01 00:34:57 +0530

    Update Quotient.c

--- Quotient.c
+++ Quotient.c
@@ -4,9 +4,11 @@
 int main(){
    int num1, num2, quot, rem;
 
+   // 1st input
    printf("Enter dividend: ");
    scanf("%d", &num1);
-
+   
+   // 2nd input 
    printf("Enter divisor: ");
    scanf("%d", &num2);
 
